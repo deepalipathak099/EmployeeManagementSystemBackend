e24e7b6f62cba2c97039cccf86d1d9344dba549f
deepalipathak099 <deepali843331@gmail.com>
2023-12-23 18:31:15 +0530

    GetMapping

--- src/main/java/com/preparation/EmployeeManagementSystem/controller/EmployeeController.java
+++ src/main/java/com/preparation/EmployeeManagementSystem/controller/EmployeeController.java
@@ -4,10 +4,9 @@ import com.preparation.EmployeeManagementSystem.model.Employee;
 import com.preparation.EmployeeManagementSystem.response.EmployeeResponse;
 import com.preparation.EmployeeManagementSystem.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ems")
@@ -18,5 +17,27 @@ public class EmployeeController {
     @PostMapping("/addemployee")
     public EmployeeResponse addEmployee(@RequestBody Employee employee){
         return employeeService.saveEmployeeDetails(employee);
+    }
+    @GetMapping("/getemployee")
+    public Employee getEmployeeById(@RequestParam int Id){
+        return  employeeService.searchEmployeeById(Id);
+    }
+    @GetMapping("/getemployee/employeeId")
+    public List<Employee> fetchEmployeeByEmployeeId(@RequestParam String employeeId){
+        return  employeeService.fetchEmployeeByEmployeeId(employeeId);
+    }
+    @GetMapping("/getemployeebyaddressandage")
+    public List<Employee> findEmployeeBasedOnAddressAndAge(@RequestParam String employeeAddress,@RequestParam int minAge,@RequestParam int maxAge){
+        return employeeService.findEmployeeBasedOnAddressAndAge(employeeAddress,minAge,maxAge);
+    }
+
+    @GetMapping("/getemployeebynameandemail")
+    public Employee findEmployeeByEmployeeNameAndEmployeeEmail(@RequestParam String employeeName,@RequestParam String employeeEmail){
+        return employeeService.findEmployeeByEmployeeNameAndEmail(employeeName,employeeEmail);
+    }
+
+    @GetMapping("/getemployeebyfirstletter")
+    public List<Employee> findEmployeeBasedOnFirstLetterOfName(@RequestParam String firstLetter){
+        return employeeService.findEmployeeBasedOnFirstLetter(firstLetter);
     }
 }
--- src/main/java/com/preparation/EmployeeManagementSystem/service/EmployeeService.java
+++ src/main/java/com/preparation/EmployeeManagementSystem/service/EmployeeService.java
@@ -6,6 +6,8 @@ import com.preparation.EmployeeManagementSystem.response.EmployeeResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class EmployeeService {
     @Autowired
@@ -18,4 +20,23 @@ public class EmployeeService {
         return employeeResponse;
 
     }
+    public Employee searchEmployeeById(int Id){
+        return employeeRepository.findById(Id).get();
+    }
+    public List<Employee> fetchEmployeeByEmployeeId(String employeeId){
+        return employeeRepository.findByEmployeeId(employeeId);
+    }
+
+    public List<Employee> findEmployeeBasedOnAddressAndAge(String employeeAddress, int minAge, int maxAge){
+        return employeeRepository.findEmployeeBasedOnAddressAndAge(employeeAddress,minAge,maxAge);
+    }
+
+    public Employee findEmployeeByEmployeeNameAndEmail(String employeeName, String employeeEmail){
+        return employeeRepository.findEmployeeByEmployeeNameAndEmail(employeeName,employeeEmail);
+    }
+
+    public List<Employee> findEmployeeBasedOnFirstLetter(String firstLetter){
+        return employeeRepository.findEmployeeByFirstLetterOfName(firstLetter);
+    }
+
 }
